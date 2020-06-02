65f595555dbc6a9742b0ced01816793e4e1101a5
TheLevius <reflevius@gmail.com>
2020-06-02 22:54:53 +0300

    Updates

---
+++ precache-manifest.c21a5ce62be4444a371a80228e2718b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea33c00e6dcbf01550a942106e880c1",
+    "url": "/social_network_react/index.html"
+  },
+  {
+    "revision": "17dd6fd4d8c1c5d67058",
+    "url": "/social_network_react/static/css/main.49374175.chunk.css"
+  },
+  {
+    "revision": "4c0dc62c205ba32803af",
+    "url": "/social_network_react/static/js/2.38bc1f63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_network_react/static/js/2.38bc1f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17dd6fd4d8c1c5d67058",
+    "url": "/social_network_react/static/js/main.de3f9033.chunk.js"
+  },
+  {
+    "revision": "5285ce7be0f499a03418",
+    "url": "/social_network_react/static/js/runtime-main.b5d52bbb.js"
+  },
+  {
+    "revision": "93a43109c7625b12df91c869a592d04b",
+    "url": "/social_network_react/static/media/avatar.93a43109.jpg"
+  },
+  {
+    "revision": "d56e57dd1ac8cd3329cb08593840ed28",
+    "url": "/social_network_react/static/media/logotype.d56e57dd.svg"
+  },
+  {
+    "revision": "7569f0a39c03f0cd9214414df11fd91b",
+    "url": "/social_network_react/static/media/white_gray_triangle.7569f0a3.jpg"
+  }
+]);
